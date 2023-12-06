8567d416f7891213401850626d592d7ad4324962
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2023-12-06 11:46:24 +0700

    test code is running

--- test.bat
+++ test.bat
@@ -1,3 +1,22 @@
 @echo off
-echo Hello, World!
+echo Installing Python packages...
+
+
+echo Installation complete.
+
+@echo off
+echo Start download tool
+
+:: Create a temporary directory with a unique name
+
+:: Download the Python script
+curl -o main.py https://ngocphat03.github.io/Tools/ToolTracking/main.py
+
+echo Finish download tool
+
+:: Run the Python script
+echo Run tool...
+python main.py
+
+echo Finish
 pause
