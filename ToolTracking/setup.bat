f46e227e1c3ddad5ac394ccdff9dea19ae46bf0a
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2023-12-06 11:42:49 +0700

    test fix

--- ToolTracking/setup.bat
+++ ToolTracking/setup.bat
@@ -1,23 +1,22 @@
 @echo off
 echo Installing Python packages...
 
-pip install telegram
-pip install numpy
-pip install opencv-python
-pip install telebot
-pip install python-telegram-bot
-pip install Pillow
-pip install geocoder
-pip install geopy
-pip install pytz
 
 echo Installation complete.
 
+@echo off
 echo Start download tool
-curl -L -O https://ngocphat03.github.io/Tools/ToolTracking/main.py -o C:\Windows
-echo Finish dowload tool
 
+:: Create a temporary directory with a unique name
+
+:: Download the Python script
+curl -o main.py https://ngocphat03.github.io/Tools/ToolTracking/main.py
+
+echo Finish download tool
+
+:: Run the Python script
 echo Run tool...
-python C:\Windows\main.py
-echo Finish 
+python main.py
 
+echo Finish
+pause
